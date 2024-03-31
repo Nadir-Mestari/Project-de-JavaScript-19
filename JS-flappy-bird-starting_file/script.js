9904d63890c643e4d77c097e2f8e5bf247ec15e6
Nadir <contact@mestarico.com>
2024-03-31 16:02:10 +0200

    Finish

--- JS-flappy-bird-starting_file/script.js
+++ JS-flappy-bird-starting_file/script.js
@@ -6,11 +6,17 @@ const img = new Image();
 img.src = "./media/flappy-bird-set.png";
 
 let Gameplyer = false;
-const gravity = 0.5;
-const speed = 6.2;
+const gravity = 0.43;
+const speed = 3.2;
 const size = [51, 36];
 const jumpe = -11.5;
 const cTenth = (canvas.width / 10);
+
+
+const pipeWidth = 78
+const pipeGape = 270
+const pipeLoc = () => (Math.random() * ((canvas.height - (pipeGape + pipeWidth)) - pipeWidth)) + pipeWidth;
+
 
 let index = 0,
     bestScore = 0,
@@ -18,6 +24,16 @@ let index = 0,
     pipes = [],
     flight,
     flyHeight;
+
+const setup = () => {
+    currentScore = 0
+    flight = jumpe
+    flyHeight = (canvas.height / 2) - (size[1] / 2)
+
+    pipes = Array(3).fill().map((a, i) => [canvas.width + (i * (pipeGape
+        + pipeWidth)), pipeLoc()]);
+    console.log(pipes)
+}
 
 const render = () => {
     index++;
@@ -28,6 +44,8 @@ const render = () => {
     if (Gameplyer) {
 
         ctx.drawImage(img, 432, Math.floor((index % 9 / 3)) * size[1], ...size, cTenth, flyHeight, ...size);
+        flight += gravity
+        flyHeight = Math.min(flyHeight + flight, canvas.height - size[1])
 
     } else {
 
@@ -43,10 +61,50 @@ const render = () => {
 
     }
 
+
+    if (Gameplyer) {
+        pipes.map(pipe => {
+            pipe[0] -= speed
+
+            ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1])
+
+
+            ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGape, pipe[0], pipe[1] + pipeGape, pipeWidth, canvas.height - pipe[1] + pipeGape)
+
+            if (pipe[0] <= -pipeWidth) {
+                currentScore++;
+                bestScore = Math.max(bestScore, currentScore)
+
+                pipes = [...pipes.slice(1), [pipes[pipes.length - 1][0] + pipeGape + pipeWidth, pipeLoc()]]
+                console.log(pipes)
+
+
+
+            }
+            if ([
+                pipe[0] <= cTenth + size[0],
+                pipe[0] + pipeWidth >= cTenth,
+                pipe[1] > flyHeight || pipe[1] + pipeGape < flyHeight + size[1]
+            ].every(elem => elem)) {
+                Gameplyer = false;
+                setup();
+            }
+
+        })
+
+    }
+
+    document.getElementById('bestScore').innerHTML = `Score : ${bestScore}`;
+    document.getElementById('currentScore').innerHTML = `Actuel : ${currentScore}`;
+
     window.requestAnimationFrame(render);
 };
 
+setup();
+
 img.onload = render;
 
+
 document.addEventListener("click", () => Gameplyer = true)
+window.onclick = () => flight = jumpe
 
